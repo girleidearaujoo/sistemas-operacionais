a9528a5357e124657772750610d0f740a4cc3773
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-12-14 11:27:27 -0300

    ...

--- projeto-final.sh
+++ projeto-final.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # LATAM
-op=$( dialog --stdout --menu 'Bem-Vindo ao serviço de Check-in e bagagem da LATAM, o que deseja?' 0 0 0 1 'Check-in' 2 'Pese sua bagagem' )
+op=$( dialog --stdout --menu 'Bem-Vindo ao serviço de Check-in,  bagagem e compra de passagens da LATAM, o que deseja?' 0 0 0 1 'Check-in' 2 'Pese sua bagagem' 3 'Compre uma passagem' )
 echo $op
 if [ $op -eq 1 ]; then
 	id=$( dialog --stdout --inputbox 'Digite seu ID:' 0 0  )
+elif [ $op -eq 3 ]; then
+	destino=$( dialog --stdout --menu 'Para onde você quer ir?' 1 'São Paulo' 2 'Rio Janeiro' 3 'Santa Catarina'  0 0 0)
+	if [ $destino -eq 1 ] then;
+		dialog --msgbox
 fi
